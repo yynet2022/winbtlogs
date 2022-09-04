009fd9a7ca765313849af6721d44965707cddb77
Yokota Yoshinori <yokota@yynet.org>
2022-09-04 09:47:16 +0900

    サンプル２追加

---
+++ sample2.bat
@@ -0,0 +1,20 @@
+@powershell -NoProfile -ExecutionPolicy Unrestricted "$s=[scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 1})-join\"`n\");&$s" %*&goto:eof
+
+#GET-WinEvent -ListProvider *
+
+$a=GET-WinEvent -FilterHashtable @{
+   LogName='System'
+   ProviderName='*Kernel-Power', '*Kernel-Boot', '*Windows-Winlogon'
+} | Where-Object{$_.Id -ne 566}
+# $a.count
+
+$b=GET-WinEvent -FilterHashtable @{
+   LogName='System'
+   Id=6005,6006,6008
+}
+# $b.count
+
+$a + $b | `
+Select-Object TimeCreated,ProviderName,Id,LevelDisplayName,Message | `
+Sort-Object -Property TimeCreated | `
+Export-CSV b.csv -NoTypeInformation -Encoding UTF8
