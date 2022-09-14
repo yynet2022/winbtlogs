68e32e24cf7c25aa97b8aa7834d82953917bad7b
Yokota Yoshinori <yokota@yynet.org>
2022-09-14 20:23:48 +0900

    Update pconoff.py & setup.vbs

--- pconoff.py
+++ pconoff.py
@@ -10,6 +10,8 @@ TABLE_NAME = 'pconoff'
 DB_NAME = f'{TABLE_NAME}_{{}}.db'
 CSV_NAME = f'{TABLE_NAME}_{{}}.csv'
 
+DB_TIMESHIFT = datetime.timedelta(hours=5)
+
 TARGET_SOURCE_NAMES = ('Microsoft-Windows-Kernel-Power',
                        'Microsoft-Windows-Kernel-Boot',
                        'Microsoft-Windows-Winlogon')
@@ -35,7 +37,7 @@ def Event2DB():
         for ee in e:
             eID = winerror.HRESULT_CODE(ee.EventID)
             if isTarget(ee.SourceName, eID):
-                dbID = ee.TimeGenerated.strftime('%Y%m')
+                dbID = (ee.TimeGenerated - DB_TIMESHIFT).strftime('%Y%m')
                 if dbID not in DBcons:
                     db = DB_NAME.format(dbID)
                     con = sqlite3.connect(db)
@@ -63,53 +65,56 @@ def Event2DB():
         con.close()
 
 
-def main():
-    Event2DB()
+def DB2CSV(q):
+    _db = DB_NAME.format(q)
+    db = f'file:{_db}?mode=rw'
+    try:
+        con = sqlite3.connect(db, uri=True)
+    except Exception:
+        return datetime.timedelta(seconds=0)
+    cur = con.cursor()
 
-    tv = dict()
+    dv = dict()
     ts = datetime.time(0, 0, 0)
     te = datetime.time(23, 59, 59)
+    for row in cur.execute(f'SELECT * FROM {TABLE_NAME} ORDER BY gtime'):
+        tt = datetime.datetime.fromisoformat(row[0]) - DB_TIMESHIFT
+        d = tt.date()
+        t = tt.time()
+        dv.setdefault(d, {'S': te, 'E': ts})
+        if t < dv[d]['S']:
+            dv[d]['S'] = t
+        if t > dv[d]['E']:
+            dv[d]['E'] = t
+    con.close()
+
+    tx = datetime.timedelta(seconds=0)
+    c = CSV_NAME.format(q)
+    with open(c, 'w', encoding='utf_8_sig') as fd:
+        for k in sorted(dv.keys()):
+            dte = datetime.datetime.combine(k, dv[k]['E']) + DB_TIMESHIFT
+            dts = datetime.datetime.combine(k, dv[k]['S']) + DB_TIMESHIFT
+            tt = dte - dts
+            s = '{},{},{},{}\n'.format(k, dts.time(), dte.time(), tt)
+            fd.write(s)
+            tx += tt
+        ss = tx.total_seconds()
+        sss = '{}:{:02d}:{:02d}'.format(
+            int(ss // 3600), int(ss % 3600 // 60), int(ss % 60))
+        fd.write(f'Total,,,{sss}\n')
+    return tx
+
+
+def main():
+    Event2DB()
 
     n = datetime.datetime.today()
     m = datetime.datetime(n.year, n.month, 1) - datetime.timedelta(days=1)
     DBs = [m.strftime('%Y%m'), n.strftime('%Y%m')]
 
+    tv = dict()
     for q in DBs:
-        _db = DB_NAME.format(q)
-        db = f'file:{_db}?mode=rw'
-        try:
-            con = sqlite3.connect(db, uri=True)
-        except Exception:
-            continue
-        cur = con.cursor()
-
-        dv = dict()
-        for row in cur.execute(f'SELECT * FROM {TABLE_NAME} ORDER BY gtime'):
-            tt = datetime.datetime.fromisoformat(row[0])
-            d = tt.date()
-            t = tt.time()
-            dv.setdefault(d, {'S': te, 'E': ts})
-            if t < dv[d]['S']:
-                dv[d]['S'] = t
-            if t > dv[d]['E']:
-                dv[d]['E'] = t
-        con.close()
-
-        tx = datetime.timedelta(seconds=0)
-        c = CSV_NAME.format(q)
-        with open(c, 'w', encoding='utf_8_sig') as fd:
-            for k in sorted(dv.keys()):
-                dte = datetime.datetime.combine(k, dv[k]['E'])
-                dts = datetime.datetime.combine(k, dv[k]['S'])
-                tt = dte - dts
-                s = '{},{},{},{}\n'.format(k, dv[k]['S'], dv[k]['E'], tt)
-                fd.write(s)
-                tx += tt
-            ss = tx.total_seconds()
-            sss = '{}:{:02d}:{:02d}'.format(
-                int(ss // 3600), int(ss % 3600 // 60), int(ss % 60))
-            fd.write(f'Total,,,{sss}\n')
-        tv[q] = tx
+        tv[q] = DB2CSV(q)
 
     s0 = tv[DBs[0]].total_seconds()
     ss0 = '{}h {:02d}m {:02d}s'.format(
@@ -126,6 +131,7 @@ def main():
         message=f'先月 ({DBs[0]}) {ss0}\n' + f'今月 ({DBs[1]}) {ss1}',
         timeout=15
     )
+
 
 if __name__ == '__main__':
     main()
--- setup.vbs
+++ setup.vbs
@@ -1,5 +1,5 @@
 Option Explicit
-Dim fsys, fdir, shell, startupPath, sc
+Dim fsys, fdir, shell, startupPath, fn, sc
 
 Set fsys = CreateObject("Scripting.FileSystemObject")
 fdir = fsys.GetParentFolderName(WScript.ScriptFullName)
@@ -7,8 +7,12 @@ fdir = fsys.GetParentFolderName(WScript.ScriptFullName)
 Set shell = WScript.CreateObject("WScript.Shell")
 startupPath = shell.SpecialFolders("Startup")
 
-Set sc = shell.CreateShortcut(startupPath + "\pconoff - ÇµÇÂÅ`Ç∆Ç©Ç¡Ç∆.lnk")
+fn = startupPath + "\pconoff - ÇµÇÂÅ`Ç∆Ç©Ç¡Ç∆.lnk"
+if fsys.FileExists(fn) then
+   fsys.DeleteFile fn
+end if
+
+Set sc = shell.CreateShortcut(fn)
 sc.TargetPath = fdir + "\pconoff.exe"
 sc.WorkingDirectory = fdir
 sc.save
-
